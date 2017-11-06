17cc94ecb78e861b51ddfe614b8c05fa3ceb9f4f
Sebastian Henschel <sebastian@tesobe.com>
2017-11-06 11:09:56 +0100

    Improved layout of authentication on home page

--- apitester/base/static/js/base.js
+++ apitester/base/static/js/base.js
@@ -2,7 +2,6 @@ $(document).ready(function($) {
 	$('#authentication-select').change(function() {
 		$('.authentication-method').hide();
 		var method = $(this).val();
-		console.log(`#authentication-${method}`);
 		$(`#authenticate-${method}`).show();
 	});
 });
