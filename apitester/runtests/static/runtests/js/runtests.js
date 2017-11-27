6326fac11f9dabb5a6483fd5a10bce055c06f521
Sebastian Henschel <sebastian@tesobe.com>
2017-11-27 10:38:56 +0100

    Fixed an XSS issue when showing response from test

--- apitester/runtests/static/runtests/js/runtests.js
+++ apitester/runtests/static/runtests/js/runtests.js
@@ -1,11 +1,15 @@
 $(function() {
+	function escapeHTML(s) {
+		return String(s).replace(/&(?!\w+;)/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+	}
+
 	function runTest(runner) {
 		var testpath = runner.data('testpath');
 		$.get(testpath, function (data) {
 			var alertType = 'success';
 			var msg = '';
 			var collapse = '';
-			var text = `<pre>${data['text']}</pre>`;
+			var text = `<pre>${escapeHTML(data['text'])}</pre>`;
 			if (!data['success']) {
 				alertType = 'danger';
 				msg = '<ul>';
@@ -15,7 +19,7 @@ $(function() {
 				msg += '</ul>';
 			} else {
 				collapse = `<button type="button" class="btn btn-xs btn-success pull-right" data-toggle="collapse" data-target="#${data['config']['operation_id']}"><span class="glyphicon glyphicon-chevron-down"></span></button>`;
-				text = `<div id="${data['config']['operation_id']}" class="collapse"><pre>${data['text']}</pre></div>`;
+				text = `<div id="${data['config']['operation_id']}" class="collapse">${text}</div>`;
 			}
 			var result = `<div class="alert alert-${alertType}"><div class="row"><div class="col-xs-10 col-sm-11">${data['config']['summary']}<br />${data['config']['urlpath']}<br />Took ${data['execution_time']} ms<br />${msg}</div><div class="col-xs-2 col-sm-1">${collapse}</div></div>${text}</div>`;
 			$(runner.find('.result')).append(result);
