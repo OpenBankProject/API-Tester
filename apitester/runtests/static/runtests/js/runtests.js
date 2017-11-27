c30af7c53701f26638c77942ff74c4549e053f6b
Sebastian Henschel <sebastian@tesobe.com>
2017-11-27 10:58:47 +0100

    Improved chevron behaviour when collapsing test results

--- apitester/runtests/static/runtests/js/runtests.js
+++ apitester/runtests/static/runtests/js/runtests.js
@@ -18,7 +18,7 @@ $(function() {
 				}
 				msg += '</ul>';
 			} else {
-				collapse = `<button type="button" class="btn btn-xs btn-success pull-right" data-toggle="collapse" data-target="#${data['config']['operation_id']}"><span class="glyphicon glyphicon-chevron-down"></span></button>`;
+				collapse = `<button type="button" class="btn btn-xs btn-success pull-right" data-toggle="collapse" data-target="#${data['config']['operation_id']}" aria-expanded="false"><span class="glyphicon glyphicon-chevron-right"></span><span class="glyphicon glyphicon-chevron-down"></span></button>`;
 				text = `<div id="${data['config']['operation_id']}" class="collapse">${text}</div>`;
 			}
 			var result = `<div class="alert alert-${alertType}"><div class="row"><div class="col-xs-10 col-sm-11">${data['config']['summary']}<br />${data['config']['urlpath']}<br />Took ${data['execution_time']} ms<br />${msg}</div><div class="col-xs-2 col-sm-1">${collapse}</div></div>${text}</div>`;
