174179b9a0a33435e7fcd7e28857d1e99039957e
hongwei <hongwei@tesobe.com>
2020-01-22 11:34:47 +0100

    fixed the source code for javaScript in website

--- apitester/runtests/static/runtests/js/runtests.js
+++ apitester/runtests/static/runtests/js/runtests.js
@@ -21,19 +21,19 @@ $(function() {
 			var alertType = 'success';
 			var msg = '';
 			var collapse = '';
-			var text = "<pre>${escapeHTML(data['text'])}</pre>";
+			var text = `<pre>${escapeHTML(data['text'])}</pre>`;
 			if (!data['success']) {
 				alertType = 'danger';
 				msg = '<ul>';
 				for (var i=0; i < data['messages'].length; i++) {
-					msg += "<li>${data['messages'][i]}</li>";
+					msg += `<li>${data['messages'][i]}</li>`;
 				}
 				msg += '</ul>';
 			} else {
-				collapse = "<button type='button' class='btn btn-xs btn-success pull-right' data-toggle='collapse' data-target='#${data['config']['operation_id']}' aria-expanded='false'><span class='glyphicon glyphicon-chevron-right'></span><span class='glyphicon glyphicon-chevron-down'></span></button>";
-				text = "<div id='${data['config']['operation_id']}' class='collapse'>${text}</div>";
+				collapse = `<button type='button' class='btn btn-xs btn-success pull-right' data-toggle='collapse' data-target='#${data['config']['operation_id']}' aria-expanded='false'><span class='glyphicon glyphicon-chevron-right'></span><span class='glyphicon glyphicon-chevron-down'></span></button>"`;
+				text = `<div id='${data['config']['operation_id']}' class='collapse'>${text}</div>`;
 			}
-			var result ="<div class='alert alert-${alertType}'><div class='row'><div class='col-xs-10 col-sm-11'>${data['config']['summary']}<br />${data['config']['urlpath']}<br />Took ${data['execution_time']} ms<br />${msg}</div><div class='col-xs-2 col-sm-1'>${collapse}</div></div>${text}</div>";
+			var result =`<div class='alert alert-${alertType}'><div class='row'><div class='col-xs-10 col-sm-11'>${data['config']['summary']}<br />${data['config']['urlpath']}<br />Took ${data['execution_time']} ms<br />${msg}</div><div class='col-xs-2 col-sm-1'>${collapse}</div></div>${text}</div>`;
 			$(runner.find('.result')).append(result);
 		});
 	}
