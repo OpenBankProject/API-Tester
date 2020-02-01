1355b238451c31059994356a257c002358717781
hongwei <hongwei@tesobe.com>
2020-02-01 10:31:18 +0100

    result text shown

--- apitester/runtests/static/runtests/js/runtests.js
+++ apitester/runtests/static/runtests/js/runtests.js
@@ -33,7 +33,7 @@ $(function() {
 				collapse = `<button type='button' class='btn btn-xs btn-success pull-right' data-toggle='collapse' data-target='#${data['config']['operation_id']}' aria-expanded='false'><span class='glyphicon glyphicon-chevron-right'></span><span class='glyphicon glyphicon-chevron-down'></span></button>`;
 				text = `<div id='${data['config']['operation_id']}' class='collapse'>${text}</div>`;
 			}
-			var result =`<div class='alert alert-${alertType}'><div class='row'><div class='col-xs-10 col-sm-11'>${data['config']['summary']}<br />${data['config']['urlpath']}<br />Took ${data['execution_time']} ms<br />${msg}</div><div class='col-xs-2 col-sm-1'>${collapse}</div></div>${text}</div>`;
+			var result =`<div class='alert alert-${alertType}'><div class='row'><div class='col-xs-10 col-sm-11'>${data['config']['summary']}<br />${data['config']['urlpath']}<br />Took ${data['execution_time']} ms<br />Status Code: ${data['status_code']}</div><div class='col-xs-2 col-sm-1'>${collapse}</div></div>${data['text']}</div>`;
 			$(runner.find('.result')).append(result);
 		});
 	}
