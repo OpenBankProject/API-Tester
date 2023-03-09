a8df7f94522a5c005af3066d08e046a830f54254
Reena  Aheer <ranireena68@gmail.com>
2023-03-09 12:06:50 +0100

    hide directlogin_and_gateway

--- apitester/base/views.py
+++ apitester/base/views.py
@@ -23,6 +23,5 @@ class HomeView(TemplateView):
             'ALLOW_GATEWAY_LOGIN': settings.ALLOW_GATEWAY_LOGIN,
             'override_css_url': settings.OVERRIDE_CSS_URL,
             'logo_url': settings.LOGO_URL,
-            "explorer_url":f'{settings.API_EXPLORER_HOST}?version=OBPv{settings.API_VERSION}&operation_id=OBPv1_4_0-getResourceDocsObp&currentTag=Documentation&locale=en_GB#OBPv1_4_0-getResourceDocsObp',
         })
         return context
